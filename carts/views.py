14dfd4adce76901cc938d44f531d19d3b7838d69
Diego <heisenberg.dq97@outlook.com>
2026-05-22 17:37:32 -0500

    Arreglar carriro a la session iniciada

--- carts/views.py
+++ carts/views.py
@@ -3,16 +3,15 @@ from django.shortcuts import render
 from .models import Cart
 
 def cart(request):
-
     user = request.user if request.user.is_authenticated else None
     cart_id = request.session.get('cart_id')
 
     if cart_id:
-        cart = Cart.objects.get(pk=cart_id)
+        cart = Cart.objects.get(cart_id=cart_id)
     else:
         cart = Cart.objects.create(user=user)
     
-    request.session['cart_id'] = cart.id
+    request.session['cart_id'] = cart.cart_id
 
     return render(request, 'carts/cart.html', {
     })
