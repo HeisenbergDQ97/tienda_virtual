1c6aae7f31338732a540df98348ccdd6e3f72890
Diego <heisenberg.dq97@outlook.com>
2026-05-26 18:36:31 -0500

    Solucionar n+1 query

--- carts/models.py
+++ carts/models.py
@@ -34,6 +34,8 @@ class Cart(models.Model):
         self.total = self.subtotal + (self.subtotal * decimal.Decimal(Cart.FEE))
         self.save()
 
+    def products_related(self):
+        return self.cartproducts_set.select_related('product')
 
 class CartProducts(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
