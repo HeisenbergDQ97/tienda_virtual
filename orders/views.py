ed508b2e8899863b4153178d380086b4f1c8fff9
Diego <heisenberg.dq97@outlook.com>
2026-05-29 17:03:32 -0500

    Agregar login required para finalizar compras

--- orders/views.py
+++ orders/views.py
@@ -5,6 +5,10 @@ from carts.utils import get_or_create_cart
 
 from .models import Order
 
+from django.contrib.auth.decorators import login_required 
+
+
+@login_required(login_url='login')
 def order(request):
     cart = get_or_create_cart(request)
     order = get_or_create_order(cart, request)
