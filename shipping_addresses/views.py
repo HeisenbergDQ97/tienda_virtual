b45900ae4a909cfef0f1b2256b1719f7966a3877
Diego <heisenberg.dq97@outlook.com>
2026-05-30 21:30:07 -0500

    Agregar listado de direcciones del usuario

--- shipping_addresses/views.py
+++ shipping_addresses/views.py
@@ -1,4 +1,7 @@
+from django.contrib import messages
+
 from django.shortcuts import render
+from django.shortcuts import redirect
 
 from .models import ShippingAddress
 
@@ -15,7 +18,17 @@ class shippingAddressListView(ListView):
         return ShippingAddress.objects.filter(user=self.request.user).order_by('-default')
     
 def create(request):
-    form = ShippingAddressForm()
+    form = ShippingAddressForm(request.POST or None)
+
+    if request.method == 'POST' and form.is_valid():
+        shipping_address = form.save(commit=False)
+        shipping_address.user = request.user
+        shipping_address.default = not ShippingAddress.objects.filter(user=request.user).exists()
+
+        shipping_address.save()
+
+        messages.success(request, 'Dirección creada exitosamente')
+        return redirect('shipping_addresses:shipping_addresses')
 
 
     return render(request, 'shipping_addresses/create.html',{
